dea7c6a100824a7dcd2f3947c0af58698906f24e
Ana Carolina Fidelis Gonçalves <acarolinafg@gmail.com>
2019-09-04 20:28:44 -0300

    Início do projeto

---
+++ README.md
@@ -0,0 +1,21 @@
+# Trabalho de Teoria dos Grafos - Parte 1
+
+**Universidade Federal de Juiz de Fora - UFJF**          
+**Instituto de Ciências Exatas**                             
+**Departamento de Ciência da Computação**                 
+**Disciplina:** DCC059 – Teoria dos Grafos Período: 2019-3  
+**Professor:** Stênio Sã Rosário Furtado Soares             
+**Aluna:**                  
+ * Ana Carolina Fidelis Gonçalves - acarolinafg@gmail.com 
+ 
+## Orientações para compilar ##
+Deve ser passado 2 argumentos para iniciar o programa, são eles:
+1. Arquivo de Entrada - Contém a instância de um grado
+2. Arquivo de Saída - Arquivo onde os resultados devem ser impressos
+
+Executar:
+1. Vai pelo terminal até a pasta que você salvou e digite para compilar:
+`g++ main.cpp -o meuPrograma`
+2. Para executar:
+`./meuPrograma grafos.txt resultados.txt`
+
---
+++ main.cpp
@@ -0,0 +1,24 @@
+/************************************************************
+ * Universidade Federal de Juiz de Fora - UFJF              *
+ * Instituto de Ciências Exatas                             *
+ * Departamento de Ciência da Computação                    *
+ * Disciplina: DCC059 – Teoria dos Grafos Período: 2019-3   *
+ * Professor: Stênio Sã Rosário Furtado Soares              *
+ * Aluna: Ana Carolina Fidelis Gonçalves                    *
+ *                                                          *
+ *                 TRABALHO PRÁTICO GRUPO 13                *
+ ************************************************************/
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/************************************************************
+ *                  PROGRAMA PRINCIPAL                      *
+ ***********************************************************/
+int main(int argc, char** argv) {
+   
+    return 0;
+}
+
