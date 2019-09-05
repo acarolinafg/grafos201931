d0181a0fbe4062793cb3974b244a53cbf1e7c845
Ana Carolina Fidelis Gonçalves <acarolinafg@gmail.com>
2019-09-05 15:34:21 -0300

    Classe Grafo

---
+++ Grafo.cpp
@@ -0,0 +1,104 @@
+/************************************************************
+ * Universidade Federal de Juiz de Fora - UFJF              *
+ * Instituto de Ciências Exatas                             *
+ * Departamento de Ciência da Computação                    *
+ * Disciplina: DCC059 – Teoria dos Grafos Período: 2019-3   *
+ * Professor: Stênio Sã Rosário Furtado Soares              *
+ * Aluna: Ana Carolina Fidelis Gonçalves                    *
+ *                                                          *
+ *                 TRABALHO PRÁTICO GRUPO 13                *
+ ************************************************************/
+
+/************************************************************
+ *             Definindo os métodos do Grafo                *
+ ***********************************************************/
+#include <valarray>
+
+#include "Grafo.h"
+#include "No.h"
+
+/**
+ * Constrtutor de um objeto Grafo
+ * @param ordem
+ * @param dirigido
+ * @param arestaPonderada
+ * @param noPonderado
+ */
+Grafo::Grafo(int ordem, bool dirigido, bool arestaPonderada, bool noPonderado) {
+    this->ordem = ordem;
+    this->dirigido = dirigido;
+    this->arestaPoderada = arestaPonderada;
+    this->noPonderado = noPonderado;
+    this->ultimoNo = this->primeiroNo = nullptr;
+    this->nArestas = 0;
+}
+
+/**
+ * Destrutor do Grafo
+ */
+Grafo::~Grafo() {
+    No *prox = this->primeiroNo;
+    
+    while(prox!= nullptr){
+        prox->removerTodasAresta();
+        No *aux = prox->getProximoNo();
+        delete prox;
+        prox = aux;
+    }
+}
+
+/**
+ * Retorna o número de nós do grafo
+ * @return int ordem
+ */
+int Grafo::getOrdem(){
+    return this->ordem;
+}
+
+/**
+ * Retorna o número de arestas do grafo
+ * @return int nArestas
+ */
+int Grafo::getNArestas(){
+    return this->nArestas;
+}
+
+/**
+ * Retorna se o grafo é ou não direcionado
+ * @return bool dirigido
+ */
+bool Grafo::getDirigido(){
+    return this->dirigido;
+}
+
+/**
+ * Retorna se o grafo é ou não é ponderado nas arestas
+ * @return bool arestaPonderada
+ */
+bool Grafo::getArestaPonderada(){
+    return this->arestaPoderada;
+}
+
+/**
+ * Retorna se o grafo é ou não ponderado nos vértices
+ * @return 
+ */
+bool Grafo::getNoPonderado(){
+    return this->noPonderado;
+}
+
+/**
+ * Retorna o ponteiro para primeira posição na lista de nós dos grafo
+ * @return 
+ */
+No *Grafo::getPrimeiroNo(){
+    return this->primeiroNo;
+}
+
+/**
+ * Retorna o ponteiro para o último nó inserido no grafo
+ * @return 
+ */
+No *Grafo::getUltimoNo(){
+    return this->ultimoNo;
+}
---
+++ Grafo.h
@@ -0,0 +1,51 @@
+/************************************************************
+ * Universidade Federal de Juiz de Fora - UFJF              *
+ * Instituto de Ciências Exatas                             *
+ * Departamento de Ciência da Computação                    *
+ * Disciplina: DCC059 – Teoria dos Grafos Período: 2019-3   *
+ * Professor: Stênio Sã Rosário Furtado Soares              *
+ * Aluna: Ana Carolina Fidelis Gonçalves                    *
+ *                                                          *
+ *                 TRABALHO PRÁTICO GRUPO 13                *
+ ************************************************************/
+
+/************************************************************
+ *              Implementação do TAD Grafo                  *
+ ***********************************************************/
+
+#ifndef GRAFO_H
+#define GRAFO_H
+#include <iostream>
+#include "Aresta.h"
+#include "Grafo.h"
+
+using namespace std;
+
+class Grafo {
+public:
+    //Construtor
+    Grafo(int ordem, bool dirigido, bool arestaPonderada, bool noPonderado);
+    //Destrutor
+    ~Grafo();
+    //Gets
+    int getOrdem();
+    int getNArestas();
+    bool getDirigido();
+    bool getArestaPonderada();
+    bool getNoPonderado();
+    No *getPrimeiroNo();
+    No *getUltimoNo();
+private:
+    //Atributos
+    int ordem;
+    int nArestas;
+    bool dirigido;
+    bool arestaPoderada;
+    bool noPonderado;
+    No* primeiroNo;
+    No* ultimoNo;
+
+};
+
+#endif /* GRAFO_H */
+
