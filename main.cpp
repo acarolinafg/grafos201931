12f7bd0906a7085674777076326fc097cd8b1367
Ana Carolina Fidelis Gonçalves <acarolinafg@gmail.com>
2019-10-03 20:46:17 -0300

    Função para escrita do grafo em um arquivo

--- main.cpp
+++ main.cpp
@@ -9,9 +9,12 @@
  *                 TRABALHO PRÁTICO GRUPO 13                *
  ************************************************************/
 
-#include <cstdlib>
 #include <iostream>
+#include <fstream>
 #include "Grafo.h"
+#include "No.h"
+#include "Aresta.h"
+
 
 using namespace std;
 
@@ -23,49 +26,96 @@ using namespace std;
  * @param noPonderado
  * @return 
  */
-Grafo *leitura(ifstream& arquivo, int dirigido, int arestaPonderada, int noPonderado){
+Grafo *leitura(ifstream& arquivo, int dirigido, int arestaPonderada, int noPonderado) {
     //Variáveis auxiliares
     int ordem;
     int idNoOrigem;
     int idNoDestino;
-    
+
     //Leitura da Ordem do grafo
     arquivo >> ordem;
-    
+
     //Criação do Grafo
     Grafo *grafo = new Grafo(dirigido, arestaPonderada, noPonderado);
-    
+
     //Leitura do arquivo
-    if(!grafo->getArestaPonderada() && !grafo->getNoPonderado()){
-        while(arquivo >> idNoOrigem >> idNoDestino){
+    if (!grafo->getArestaPonderada() && !grafo->getNoPonderado()) {
+        while (arquivo >> idNoOrigem >> idNoDestino) {
             grafo->inserirAresta(idNoOrigem, idNoDestino, 0);
         }
-    }else if(grafo->getArestaPonderada() && !grafo->getNoPonderado()){
+    } else if (grafo->getArestaPonderada() && !grafo->getNoPonderado()) {
         float pesoAresta;
-        
-        while(arquivo >> idNoOrigem >> idNoDestino >> pesoAresta){
+
+        while (arquivo >> idNoOrigem >> idNoDestino >> pesoAresta) {
             grafo->inserirAresta(idNoOrigem, idNoDestino, pesoAresta);
         }
-    }else if(!grafo->getNoPonderado() && !grafo->getArestaPonderada()){
+    } else if (grafo->getNoPonderado() && !grafo->getArestaPonderada()) {
         float pesoNoOrigem, pesoNoDestino;
-        
-        while(arquivo >> idNoOrigem >> pesoNoOrigem >> idNoDestino >> pesoNoDestino){
+
+        while (arquivo >> idNoOrigem >> pesoNoOrigem >> idNoDestino >> pesoNoDestino) {
             grafo->inserirAresta(idNoOrigem, idNoDestino, 0);
             grafo->getNo(idNoOrigem)->setPeso(pesoNoOrigem);
             grafo->getNo(idNoDestino)->setPeso(pesoNoDestino);
         }
-    }else if(grafo->getArestaPonderada() && grafo->getNoPonderado()){
+    } else if (grafo->getArestaPonderada() && grafo->getNoPonderado()) {
         float pesoNoOrigem, pesoNoDestino, pesoAresta;
-        
-        while(arquivo >> idNoOrigem >> pesoNoOrigem >> idNoDestino >> pesoNoDestino >> pesoAresta){
+
+        while (arquivo >> idNoOrigem >> pesoNoOrigem >> idNoDestino >> pesoNoDestino >> pesoAresta) {
             grafo->inserirAresta(idNoOrigem, idNoDestino, pesoAresta);
             grafo->getNo(idNoOrigem)->setPeso(pesoNoOrigem);
             grafo->getNo(idNoDestino)->setPeso(pesoNoDestino);
         }
     }
-    
+
     return grafo;
 }
+
+/**
+ * Grava um grafo em um arquivo
+ * @param arquivo
+ * @param grafo
+ */
+void escrita(ofstream& arquivo, Grafo *grafo) {
+    if (grafo != nullptr && grafo->getOrdem() > 0) {
+        //Gravando a ordem do grafo
+        arquivo << grafo->getOrdem();
+
+        //Percorrendo o grafo
+        No *p = grafo->getPrimeiroNo();
+        Aresta * a;
+
+        while (p != nullptr) {
+            a = p->getPrimeiraAresta();
+
+            while (a != nullptr) {
+                if (!grafo->getArestaPonderada() && !grafo->getNoPonderado()) {
+                    arquivo << p->getId() << a->getLabel();
+                } else if (grafo->getArestaPonderada() && !grafo->getNoPonderado()) {
+                    arquivo << p->getId() << a->getLabel() << a->getPeso();
+                } else if (grafo->getNoPonderado() && !grafo->getArestaPonderada()) {
+                    float pesoNoOrigem, pesoNoDestino;
+                    pesoNoOrigem = p->getPeso();
+                    pesoNoDestino = grafo->getNo(a->getLabel())->getPeso();
+
+                    arquivo << p->getId() << pesoNoOrigem << a->getLabel() << pesoNoDestino;
+                } else if (grafo->getArestaPonderada() && grafo->getNoPonderado()) {
+                    float pesoNoOrigem, pesoNoDestino, pesoAresta;
+                    pesoNoOrigem = p->getPeso();
+                    pesoNoDestino = grafo->getNo(a->getLabel())->getPeso();
+                    pesoAresta = a->getPeso();
+
+                    arquivo << p->getId() << pesoNoOrigem << a->getLabel() << pesoNoDestino << pesoAresta;
+                }
+
+                a = a->getProximaAresta();
+            }
+
+            p = p->getProximoNo();
+        }
+    }
+}
+
+
 
 /************************************************************
  *                  PROGRAMA PRINCIPAL                      *
