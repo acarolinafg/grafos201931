8b76ecd74a40d1dfa94b47e3e9576e0df66ecba2
Ana Carolina Fidelis Gonçalves <acarolinafg@gmail.com>
2019-11-22 07:11:16 -0300

    Criação da classe para os algoritimos construtivos do problema Conjunto Dominante Conexo

---
+++ CojuntoDominanteConexo.cpp
@@ -0,0 +1,24 @@
+/************************************************************
+ * Universidade Federal de Juiz de Fora - UFJF              *
+ * Instituto de Ciências Exatas                             *
+ * Departamento de Ciência da Computação                    *
+ * Disciplina: DCC059 – Teoria dos Grafos Período: 2019-3   *
+ * Professor: Stênio Sã Rosário Furtado Soares              *
+ * Aluno(s): Ana Carolina Fidelis Gonçalves                 *
+ *           Paulo Sergio de Oliveira Silva                 *
+ *                                                          *
+ *                 TRABALHO PRÁTICO GRUPO 13                *
+ ************************************************************/
+
+/***********************************************************
+ *          Problema - Subconjunto Dominate Conexo         *
+ ***********************************************************/
+
+#include "CojuntoDominanteConexo.h"
+
+CojuntoDominanteConexo::CojuntoDominanteConexo() {
+}
+
+CojuntoDominanteConexo::~CojuntoDominanteConexo() {
+}
+
---
+++ CojuntoDominanteConexo.h
@@ -0,0 +1,29 @@
+/************************************************************
+ * Universidade Federal de Juiz de Fora - UFJF              *
+ * Instituto de Ciências Exatas                             *
+ * Departamento de Ciência da Computação                    *
+ * Disciplina: DCC059 – Teoria dos Grafos Período: 2019-3   *
+ * Professor: Stênio Sã Rosário Furtado Soares              *
+ * Aluno(s): Ana Carolina Fidelis Gonçalves                 *
+ *           Paulo Sergio de Oliveira Silva                 *
+ *                                                          *
+ *                 TRABALHO PRÁTICO GRUPO 13                *
+ ************************************************************/
+
+/***********************************************************
+ *          Problema - Subconjunto Dominate Conexo         *
+ ***********************************************************/
+
+#ifndef COJUNTODOMINANTECONEXO_H
+#define COJUNTODOMINANTECONEXO_H
+
+class CojuntoDominanteConexo {
+public:
+    CojuntoDominanteConexo();
+    virtual ~CojuntoDominanteConexo();
+private:
+
+};
+
+#endif /* COJUNTODOMINANTECONEXO_H */
+
